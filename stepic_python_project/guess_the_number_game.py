867b0bee4480239923eb0f381518e8e8c9f8fd5d
eeisler <eeislerr@gmail.com>
2023-04-15 23:44:18 +0500

    almost completed

--- stepic_python_project/guess_the_number_game.py
+++ stepic_python_project/guess_the_number_game.py
@@ -1,32 +1,32 @@
 from random import randint
 
-def is_valid_answer(num):
-    if num.isdigit() and float(num) - int(float(num)) == 0 and 1 <= int(num) <= 100:
+def is_valid_answer(num, x, y):
+    if num.isdigit() and float(num) - int(float(num)) == 0 and x <= int(num) <= y:
         return True
     else:
         return False
 
 def start_game():
-    print("This is GuessTheNumber game."
-          "\nThe rules are simple:"
-          "\nWe generate a random number in a range which you can choose and then you guess it. "
-          "\nYou'll be given a hints if your number is bigger or smaller than the right answer"
-          "\nEnter the range:")
-
-    while True: #todo - after n in continue the game continues to work
+    print("Hello! This is GuessTheNumber game.")
+    while True:
+        print("Enter the range to guess the number: ")# if 1 - yes 2 - no (continue) works again
         x, y = input(), input()
         if x.isdigit() and y.isdigit():
             if int(x) > int(y):
                 x, y = y, x
         print(f"Guess the number between {x} and {y}: ")
         compare_numbers(x, y)
+        if continue_game():
+            continue
+        else:
+            break
 
 def compare_numbers(x, y):
     count = 1
     answer = randint(int(x), int(y))
     while True:
         user_answer = input()
-        if is_valid_answer(user_answer) == True:
+        if is_valid_answer(user_answer, int(x), int(y)) == True:
             if int(user_answer) > answer:
                 print("Smaller")
                 count += 1
@@ -35,19 +35,19 @@ def compare_numbers(x, y):
                 count += 1
             elif int(user_answer) == answer:
                 print(f"You won! Count of tries: {count}")
-                continue_game()
                 break
         else:
             print("Incorrect number try again: ")
             continue
 def continue_game():
-    print("Do you wanna continue to play? y/n")
-    user_answer = input()
+    user_answer = input("Do you wanna continue to play? y/n \n")
     if user_answer in ("y", "n"):
         if user_answer == "y":
             start_game()
+            return True
         elif user_answer == "n":
             print("See you later")
+            return False
     else:
         print("Invalid command. Try again. y or n?")
 
