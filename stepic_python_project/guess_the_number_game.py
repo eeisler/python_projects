7c31102692e6811dca4a7f93b46862ed09705821
eeisler <eeislerr@gmail.com>
2023-04-16 15:37:12 +0500

    complete game and formatted

--- stepic_python_project/guess_the_number_game.py
+++ stepic_python_project/guess_the_number_game.py
@@ -1,10 +1,9 @@
 from random import randint
 
 def is_valid_answer(num, x, y):
-    if num.isdigit() and float(num) - int(float(num)) == 0 and x <= int(num) <= y:
-        return True
-    else:
-        return False
+    return bool(num.isdigit() and float(num) - int(float(num)) == 0 and x <= int(num) <= y)
+
+
 def start_game():
     print("Hello! This is GuessTheNumber game.")
     while True:
@@ -15,16 +14,15 @@ def start_game():
                 x, y = y, x
         print(f"Guess the number between {x} and {y}: ")
         compare_numbers(x, y)
-        if continue_game():
-            continue
-        else:
-            break
+        if not continue_game(): break
+
+
 def compare_numbers(x, y):
     answer = randint(int(x), int(y))
     count = 1
     while True:
         user_answer = input()
-        if is_valid_answer(user_answer, int(x), int(y)) == True:
+        if is_valid_answer(user_answer, int(x), int(y)):
             if int(user_answer) > answer:
                 print("Smaller")
                 count += 1
@@ -37,14 +35,18 @@ def compare_numbers(x, y):
         else:
             print("Incorrect number try again: ")
             continue
+
+
 def continue_game():
     user_answer = input("Do you wanna continue to play? y/n\n")
     while True:
-        if user_answer not in ("y", "n", "Y", "N"):
+        if user_answer.lower() not in ("y", "n"):
             user_answer = input("Invalid command. Try again. y or n?")
-        elif user_answer in ("n", "N"):
+        elif user_answer.lower() in ("n"):
             print("See you later")
             return False
         else:
             return True
+
+
 start_game()
--- stepic_python_project/magic_ball.py
+++ stepic_python_project/magic_ball.py
@@ -6,28 +6,33 @@ answer_list = ["Definitely", "Determined", "No doubt", "Definitely yes", "You ca
                "Don't even think", "My answer is no", "According to my information, no", "Prospects are not very good", "Very doubtful"]
 ask_question_list = ["What is your question?", "What question is bothering you, my friend?", "What do you want to know?",
                      "You came to me with a question, for your future is vague. Ask a question and you will get an answer."]
+
+
 def greetings():
     print("Hello, my curious friend, I am a magic ball and I know the answer to any of your questions.")
     user_name = input("But first, let me know your name\n")
     print(f"Hm.. {user_name}.. What a beautiful name)")
     game()
+
+
 def game():
     while True:
         question = input(f"{choice(ask_question_list)}\n")
         print(choice(answer_list))
-        if continue_game():
-            continue
-        else:
-            break
+        if not continue_game(): break
+
+
 def continue_game():
     answer = input("Do you want to know something else, my friend?\n")
     while True:
-        if answer not in ("no", "NO", "n", "N", "not really", "NOT REALLY",
-                      "yes", "YES", "y", "Y", "yes please", "YES PLEASE"):
+        if answer.lower not in ("no", "n", "not really",
+                      "yes", "y", "yes please"):
             answer = input("I don't understand you...Try to repeat, please\n")
-        if answer in ("no", "NO", "n", "N", "not really", "NOT REALLY"):
+        if answer.lower() in ("no", "n", "not really"):
             print("Well then, see you later!")
             return False
         else:
             return True
+
+
 greetings()
