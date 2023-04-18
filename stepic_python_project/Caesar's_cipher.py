6921fc14d35f9f30b27ea0d561b0e416389223f0
eeisler <eeislerr@gmail.com>
2023-04-18 21:57:26 +0500

    complete

--- stepic_python_project/Caesar's_cipher.py
+++ stepic_python_project/Caesar's_cipher.py
@@ -11,28 +11,27 @@ lang = input("Enter the language: English(en) or Russian(ru):\n")
 while lang != 'en' and lang != 'ru':
     lang = input("Something went wrong! You can enter only 'en' or 'ru'\n")
 
-step = input("Enter shift step(number):\n")
-while not step.isdigit():
-    step = input("Something went wrong! You can enter only a number\n")
+steps = input("Enter shift step(number):\n")
+while not steps.isdigit():
+    steps = input("Something went wrong! You can enter only a number\n")
 
-text = input("Enter the text:\n")
-while text.isspace():
-    text = input("Something went wrong! Enter the text\n")
+in_text = input("Enter the text:\n")
+while in_text.isspace():
+    in_text = input("Something went wrong! Enter the text\n")
+
+if lang == 'en':
+    capacity = 26
+    low_alphabet = eng_lower_alphabet
+    upp_alphabet = eng_upper_alphabet
+
+elif lang == 'ru':
+    capacity = 32
+    low_alphabet = rus_lower_alphabet
+    upp_alphabet = rus_upper_alphabet
 
 
-def encrypt(text, lang, step):
+def encrypt(text, step):
     result = ''
-
-    if lang == 'en':
-        capacity = 26
-        low_alphabet = eng_lower_alphabet
-        upp_alphabet = eng_upper_alphabet
-
-    elif lang == 'ru':
-        capacity = 32
-        low_alphabet = rus_lower_alphabet
-        upp_alphabet = rus_upper_alphabet
-
     for i in range(len(text)):
         if text[i].isalpha():
             if text[i].islower():
@@ -43,15 +42,25 @@ def encrypt(text, lang, step):
                 result += upp_alphabet[(place + int(step)) % capacity]
         else:
             result += text[i]
+    return print(f"The result: {result}")
 
-    return print(result)
 
-
-def decrypt(text, lang, step):
-    pass
+def decrypt(text, step):
+    result = ''
+    for i in range(len(text)):
+        if text[i].isalpha():
+            if text[i].islower():
+                place = low_alphabet.index(text[i])
+                result += low_alphabet[(place - int(step)) % capacity]
+            elif text[i].isupper():
+                place = upp_alphabet.index(text[i])
+                result += upp_alphabet[(place - int(step)) % capacity]
+        else:
+            result += text[i]
+    return print(f"The result: {result}")
 
 
 if action == 'e':
-    encrypt(text, lang, step)
+    encrypt(in_text, steps)
 elif action == 'd':
-    decrypt(text, lang, step)
+    decrypt(in_text, steps)
